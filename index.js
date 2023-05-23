2708a06d6fc0aaf53eaf14b397144c5fa5ebff18
Verbinding Maken <verbindingmaken@outlook.com>
2023-05-23 10:36:11 +0200

    Resize selection box, keep selection box square, crop image based on selection

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-
+// Elements
 const fileInput = document.querySelector(".image-upload__input")
 const uploadButton = document.querySelector(".image-upload__upload-button");
 const downloadButton = document.querySelector('.image-edit__download-button');
@@ -7,24 +7,31 @@ const cropButton = document.querySelector('.image-edit__crop-button');
 const resetButton = document.querySelector('.image-edit__reset-button')
 const canvasFrame = document.querySelector('.image-edit-canvas-frame');
 const selectionBox = document.querySelector('.image-edit__selection-box');
+const resizeHandleSelectionBox = document.querySelector('.image-edit__resize-handle-selection-box');
 
+// Event listeners on elements
 fileInput.addEventListener('change', onFileSelected);
 uploadButton.addEventListener('click', selectImageForUpload);
 downloadButton.addEventListener('click', downloadImage);
 cropButton.addEventListener('click', cropImage);
 resetButton.addEventListener('click', resetImage);
-selectionBox.addEventListener('mousedown', clickSelectionBox)
+selectionBox.addEventListener('mousedown', clickSelectionBox);
+// Prevent browser default dragstart event
 selectionBox.addEventListener('dragstart', () => false);
+resizeHandleSelectionBox.addEventListener('mousedown', clickResizeHandleSelectionBox);
+resizeHandleSelectionBox.addEventListener('dragstart', () => false);
 
+// Image canvas
 const canvas = document.querySelector('.image-edit__canvas');
 const canvasContext = canvas.getContext("2d");
 
-// Image and image information
+// Image and image information, set after image is selected; 
 const originalImage = {
     name: "",
     size: 0,
     width: 0,
     height: 0,
+    scale: 0,
     element: new Image()
 }
 
@@ -35,7 +42,8 @@ let selectionBoxDimensions = {
     height: 0
 }
 
-let imageLoaded = false;
+let isImageLoaded = false;
+let isSelectionResizing = false
 
 function selectImageForUpload() {
     fileInput.click();
@@ -58,29 +66,29 @@ function displayImage(imageFile) {
         originalImage.width = originalImage.element.naturalWidth;
         originalImage.height = originalImage.element.naturalHeight; ``
 
-        console.log(originalImage);
-
         canvas.width = originalImage.width;
         canvas.height = originalImage.height;
-
-        console.log('canvas: ', canvas.width, canvas.height, canvas);
 
         canvas.style.maxWidth = '300px';
         canvas.style.maxHeight = '300px';
 
         canvasContext.drawImage(originalImage.element, 0, 0, originalImage.width, originalImage.height);
 
-        selectionBox.style.display = "block";
-        imageLoaded = true;
-    });
+        originalImage.scale = originalImage.width / canvas.offsetWidth;
 
+        selectionBox.style.display = "block";
+        resizeHandleSelectionBox.style.display = "block";
+
+        isImageLoaded = true;
+    });
 }
 
 function downloadImage() {
-    if (imageLoaded) {
+
+    if (isImageLoaded) {
         const temporaryLink = document.createElement('a');
 
-        temporaryLink.download = `${originalImage.name}-download-resize-2.jpg`;
+        temporaryLink.download = `${originalImage.name}-${selectionBoxDimensions.width}-${selectionBoxDimensions.height}.jpg`;
         temporaryLink.href = canvas.toDataURL("image/jpeg", 1);
 
         temporaryLink.click();
@@ -88,8 +96,8 @@ function downloadImage() {
 }
 
 function resizeImage(x, y, width, heigth) {
-    if (imageLoaded) {
-        console.log('resize with: ', x, y, width, heigth);
+
+    if (isImageLoaded) {
 
         canvas.width = width;
         canvas.height = heigth;
@@ -104,56 +112,97 @@ function resizeImage(x, y, width, heigth) {
 }
 
 function cropImage() {
-    const newHeight = 150;
-    const newWidth = Math.floor(150 * (originalImage.width / originalImage.height));
 
-    resizeImage(1500, 2500, newWidth, newHeight);
+    resizeImage(
+        selectionBoxDimensions.x,
+        selectionBoxDimensions.y,
+        selectionBoxDimensions.width,
+        selectionBoxDimensions.height
+    )
+
+    selectionBox.style.display = "none";
+    resizeHandleSelectionBox.style.display = "none";
 }
 
 function resetImage() {
+
     resizeImage(0, 0, originalImage.width, originalImage.height);
+
+    selectionBox.style.display = "block";
+    resizeHandleSelectionBox.style.display = "block";
 }
+
+
 
 function clickSelectionBox(clickEvent) {
 
-    console.log('mouseclick')
+    if (!isSelectionResizing) {
 
-    const pointerX = clickEvent.offsetX;
-    const pointerY = clickEvent.offsetY;
+        const pointerX = clickEvent.offsetX;
+        const pointerY = clickEvent.offsetY;
 
-    function moveSelectionBox(moveEvent) {
 
-        const rectangle = canvasFrame.getBoundingClientRect();
-        const left = Math.floor((moveEvent.clientX - pointerX) - rectangle.left);
-        const top = Math.floor((moveEvent.clientY - pointerY) - rectangle.top);
 
-        selectionBox.style.left = `${left}px`;
-        selectionBox.style.top = `${top}px`;
+        function moveSelectionBox(moveEvent) {
 
-        const scale = originalImage.width / canvas.offsetWidth;
+            const rectangle = canvas.getBoundingClientRect();
+            const left = Math.floor((moveEvent.clientX - pointerX) - rectangle.left);
+            const top = Math.floor((moveEvent.clientY - pointerY) - rectangle.top);
 
-        selectionBoxDimensions = {
-            x: left * scale,
-            y: top * scale,
-            width: (moveEvent.target.clientWidth + 2 * moveEvent.target.clientLeft) * scale,
-            height: (moveEvent.target.clientHeight + 2 * moveEvent.target.clientTop) * scale
+            selectionBox.style.left = `${left}px`;
+            selectionBox.style.top = `${top}px`;
+
+            selectionBoxDimensions = {
+                x: Math.floor(left * originalImage.scale),
+                y: Math.floor(top * originalImage.scale),
+                width: Math.floor((moveEvent.target.clientWidth + 2 * moveEvent.target.clientLeft) * originalImage.scale),
+                height: Math.floor((moveEvent.target.clientHeight + 2 * moveEvent.target.clientTop) * originalImage.scale)
+            }
         }
 
-        console.log("Box: ", selectionBoxDimensions);
+        function stopSelection(stopEvent) {
+
+            document.onmousemove = null;
+            document.onmouseup = null;
+            selectionBox.removeEventListener('mouseup', stopSelection);
+        }
+
+        document.onmousemove = (event) => moveSelectionBox(event);
+        document.onmouseup = (event) => stopSelection(event);
+    }
+}
+
+function clickResizeHandleSelectionBox(clickEvent) {
+
+    isSelectionResizing = true;
+    document.onmousemove = null;
+
+    const pointerX = clickEvent.clientX;
+    const pointerY = clickEvent.clientY;
+
+    const rectangle = selectionBox.getBoundingClientRect();
+
+    function resizeSelectionBox(resizeEvent) {
+
+        const newWidth = Math.floor(rectangle.width + resizeEvent.clientX - pointerX);
+        const newHeight = Math.floor(rectangle.height + resizeEvent.clientY - pointerY);
+
+        const newSize = newWidth < newHeight ? newWidth : newHeight;
+
+        selectionBox.style.width = `${newSize}px`;
+        selectionBox.style.height = `${newSize}px`
+
+        selectionBoxDimensions.width = Math.floor(newSize * originalImage.scale);
+        selectionBoxDimensions.height = Math.floor(newSize * originalImage.scale);
     }
 
-    function stopSelection() {
-        resizeImage(
-            selectionBoxDimensions.x,
-            selectionBoxDimensions.y,
-            selectionBoxDimensions.width,
-            selectionBoxDimensions.height
-        )
-        selectionBox.removeEventListener('mousemove', moveSelectionBox);
-        selectionBox.removeEventListener('mouseup', stopSelection);
+    function stopResizingSelectionBox(stopEvent) {
+
+        document.onmousemove = null;
+        document.onmouseup = null;
+        isSelectionResizing = false;
     }
 
-
-    selectionBox.addEventListener('mousemove', moveSelectionBox);
-    selectionBox.addEventListener('mouseup', stopSelection);
+    document.onmousemove = (event) => resizeSelectionBox(event);
+    document.onmouseup = (event) => stopResizingSelectionBox(event);
 }
